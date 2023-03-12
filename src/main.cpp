990551e7e9ee9c2fac3c8c575339317a7b315fc0
Johannes Nielsen <flykasual@gmail.com>
2023-03-12 19:20:03 +0100

    [CLEANUP] Fix formatting
    Signed-off-by: Johannes Nielsen <flykasual@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,18 @@
  * Copyright 2023 Johannes Nielsen <flykasual@gmail.com>
  */
 /*!
- * This file is part of the game_of_life_cpp project.
- *
- * The game_of_life_cpp project is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at
- * your option) any later version.
- *
- * The game_of_life_cpp project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along with the game_of_life_cpp project. If not, see <https://www.gnu.org/licenses/>.
- */
+  * This file is part of the game_of_life_cpp project.
+  *
+  * The game_of_life_cpp project is free software: you can redistribute it and/or modify it under the terms of the
+  * GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at
+  * your option) any later version.
+  *
+  * The game_of_life_cpp project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+  * for more details.
+  *
+  * You should have received a copy of the GNU General Public License along with the game_of_life_cpp project. If not, see <https://www.gnu.org/licenses/>.
+  */
 
 #include <iostream>
 #include <memory>
@@ -32,34 +32,44 @@
 
 int main() {
   KleinBottleTopology top;
-  CellularAutomaton c{top, 40, 80};
-  CellularAutomatonPrinter cp{c};
+  CellularAutomaton c{ top, 120, 80 };
+  CellularAutomatonPrinter cp{ c };
 
-  // LambdaBasedRule killEverything{
-  //     [](const Cell& c, const CellularAutomaton& aut) {
-  //         return Cell::DEAD;
-  //     },
-  //     []() { return true; }
-  // };
+  std::vector<std::vector<int>> liveSpanMap(
+    c.getHeight(),
+    std::vector<int>(
+      c.getWidth(),
+      0
+    )
+  );
 
   // bool force = false;
-  // Cell::State forcedState = Cell::ALIVE;
+  // int maxAge = 40;
 
-  // LambdaBasedRule forceEveryThingToLiveAfter10Generations{
-  //     [&forcedState, &force](const Cell& c, const CellularAutomaton& aut) {
-  //         if (aut.getIteration() >= 10) {
-  //             force = true;
-  //         }
-  //         return forcedState;
-  //     },
-  //     [&force]() {
-  //         return force;
-  //     }
-  // };
+  c
+    // .addRule(
+    //   std::make_shared<LambdaBasedRule>(
+    //     [&liveSpanMap, &force, maxAge](const Cell& c, const CellularAutomaton& aut) {
+    //       force = false;
+    //       std::array<int, 2> pos{aut.getPosition(c)};
+    //       if (c.getState() == Cell::ALIVE) {
+    //         ++liveSpanMap[pos[0]][pos[1]];
+    //         if (liveSpanMap[pos[0]][pos[1]] >= maxAge) {
+    //           force = true;
+    //           return Cell::DEAD;
+    //         }
+    //         return Cell::ALIVE;
+    //       }
+    //       liveSpanMap[pos[0]][pos[1]] = 0;
 
-  c.addRule(std::make_shared<ConwaysRule>());
-    // .addRule(std::make_shared<LambdaBasedRule>(forceEveryThingToLiveAfter10Generations))
-    // .addRule(std::make_shared<LambdaBasedRule>(killEverything));
+    //       return Cell::DEAD;
+    //     },
+    //     [&force]() {
+    //       return force;
+    //     }
+    //   )
+    // )
+    .addRule(std::make_shared<ConwaysRule>());
 
   std::cout << cp;
   while (true) {
@@ -69,5 +79,5 @@ int main() {
     std::cout << cp;
   }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
